26bea2a98569d5bea2448895b60275c03c258429
RJ Mendiola <mrobjustin@gmail.com>
2026-08-27 01:03:11 +0800

    Added a moderator option

--- backend/database/migration_add_role.sql
+++ backend/database/migration_add_role.sql
@@ -1,7 +1,7 @@
 -- migration_add_roles.sql
 
 ALTER TABLE `admin`
-ADD COLUMN IF NOT EXISTS `role` ENUM('admin', 'super admin') NOT NULL DEFAULT 'admin' AFTER `password`,
+ADD COLUMN IF NOT EXISTS `role` ENUM('admin', 'super admin', 'moderator') NOT NULL DEFAULT 'admin' AFTER `password`,
 ADD COLUMN IF NOT EXISTS `can_add` TINYINT(1) NOT NULL DEFAULT 1 AFTER `role`,
 ADD COLUMN IF NOT EXISTS `can_edit` TINYINT(1) NOT NULL DEFAULT 1 AFTER `can_add`,
 ADD COLUMN IF NOT EXISTS `can_delete` TINYINT(1) NOT NULL DEFAULT 1 AFTER `can_edit`;
